28fdee32517a403bde7ac8d22aadbe64f3399cc9
trieu2003 <120072804+trieu2003@users.noreply.github.com>
2024-06-02 03:24:45 +0700

    Add files via upload

---
+++ app/src/main/java/com/appxemphim/data/LoginRequest.java
@@ -0,0 +1,27 @@
+package com.appxemphim.data;
+//triệu thêm
+public class LoginRequest {
+    private String emailOrUsername;
+    private String password;
+
+    public LoginRequest(String emailOrUsername, String password) {
+        this.emailOrUsername = emailOrUsername;
+        this.password = password;
+    }
+
+    public String getEmailOrUsername() {
+        return emailOrUsername;
+    }
+
+    public void setEmailOrUsername(String emailOrUsername) {
+        this.emailOrUsername = emailOrUsername;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
