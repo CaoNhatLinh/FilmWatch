6cec555788b57d7e05be6614df8d5f6c27ff5463
BaoBao <dinhb140@gmail.com>
2024-06-07 14:28:18 +0700

    update chi tiet xem phim

--- app/src/main/java/com/appxemphim/activities/ChiTietPhimActivity.java
+++ app/src/main/java/com/appxemphim/activities/ChiTietPhimActivity.java
@@ -3,6 +3,7 @@ package com.appxemphim.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,6 +39,21 @@ public class ChiTietPhimActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Không tìm thấy mã phim", Toast.LENGTH_SHORT).show();
         }
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        back();
+    }
+    public void back()
+    {
+        ImageView back = findViewById(R.id.ivBack);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
     private void fetchPhimDetails(int maPhim) {
         PhimDAO phimDAO = new PhimDAO();
